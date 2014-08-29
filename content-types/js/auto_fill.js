20a925ab4d53ff19729867a00fe4346e0d4b8cc6
Alexx G <alexxgermann@yandex.ru>
2014-08-29 17:26:34 +0300

    small fixes to issue #3

--- content-types/js/auto_fill.js
+++ content-types/js/auto_fill.js
@@ -4,8 +4,8 @@ jQuery(document).ready(function($) {
 
 		var plural_name = $(this).val();
 
-		$('[name="labels[menu_name]"').val( plural_name );
-		$('[name="labels[all_items]"').val( translations_js.all_ct + ' ' + plural_name );
+		$('[name="labels[menu_name]"]').val( plural_name );
+		$('[name="labels[all_items]"]').val( translations_js.all_ct + ' ' + plural_name );
 
 	});
 
@@ -13,12 +13,12 @@ jQuery(document).ready(function($) {
 
 		var singular_name = $(this).val();
 
-		$('[name="labels[view_item]"').val( translations_js.view_ct + ' ' + singular_name );
-		$('[name="labels[add_new_item]"').val( translations_js.add_new_ct + ' ' + singular_name );
-		$('[name="labels[add_new]"').val( translations_js.add_ct + ' ' + singular_name );
-		$('[name="labels[edit_item]"').val( translations_js.edit_ct + ' ' + singular_name );
-		$('[name="labels[update_item]"').val( translations_js.update_ct + ' ' + singular_name );
-		$('[name="labels[parent_item_colon]"').val( translations_js.parent_ct + ' ' + singular_name );
+		$('[name="labels[view_item]"]').val( translations_js.view_ct + ' ' + singular_name );
+		$('[name="labels[add_new_item]"]').val( translations_js.add_new_ct + ' ' + singular_name );
+		$('[name="labels[add_new]"]').val( translations_js.add_ct + ' ' + singular_name );
+		$('[name="labels[edit_item]"]').val( translations_js.edit_ct + ' ' + singular_name );
+		$('[name="labels[update_item]"]').val( translations_js.update_ct + ' ' + singular_name );
+		$('[name="labels[parent_item_colon]"]').val( translations_js.parent_ct + ' ' + singular_name );
 	});
 
 });
